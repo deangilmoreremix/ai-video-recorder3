9ba7077c7d6c5de138a55ce2d53a4fa4c53f17f3
Dean Gilmore <dean@videoremix.io>
2025-05-09 23:13:19 -0400

    Fix useRef not defined error

--- src/hooks/useAIFeatures.ts
+++ src/hooks/useAIFeatures.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useRef } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as blazeface from '@tensorflow-models/blazeface';
 import * as bodySegmentation from '@tensorflow-models/body-segmentation';
